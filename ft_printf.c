0da5d5a64bdaedeed9c15e2958f9b7bf28fd1379
Malik Jean <maljean@e1z3r6p8.42.us.org>
2018-05-30 02:21:04 -0700

    Messing with ld

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: maljean <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/29 23:48:08 by maljean           #+#    #+#             */
-/*   Updated: 2018/05/30 02:19:14 by maljean          ###   ########.fr       */
+/*   Updated: 2018/05/30 02:20:56 by maljean          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -502,6 +502,7 @@ int	ft_putwstr(wchar_t *ws)
 		ft_putchar(ws[i]);
 		i++;
 	}
+	ft_putchar(ws[i]);
 	return (i);
 }
 
