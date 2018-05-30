8ab1ad18b96dc27397eb0f912fef71fd4ed0bfb6
Malik Jean <maljean@e1z3r6p8.42.us.org>
2018-05-30 00:52:02 -0700

    Taking a break to norme

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: maljean <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/29 23:48:08 by maljean           #+#    #+#             */
-/*   Updated: 2018/05/30 00:48:26 by maljean          ###   ########.fr       */
+/*   Updated: 2018/05/30 00:51:54 by maljean          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -340,24 +340,16 @@ int	ft_parse(char *s, int *i, t_ops *ops)
 	return ((ops->w <= 1 && ops->p <= 1 && ops->m <= 1 && ops->c == 1) ? 1 : 0);
 }
 
-static	int	get_unumlen(size_t num, int base)
-{
-	int	i;
-
-	i = 1;
-	while (num /= base)
-		i++;
-	return (i);
-}
-
 char		*ft_itoabase_umax(size_t num, int base, t_ops *ops)
 {
 	char			*str;
 	int				len;
 	char			*basestr;
 
+	len = 0;
+	while (num /= base)
+		len++;
 	basestr = ft_strdup("0123456789abcdef");
-	len = get_unumlen(num, base);
 	ops->num += 1;
 	if (!(str = (char *)malloc(sizeof(*str) * len + 1)))
 	{
