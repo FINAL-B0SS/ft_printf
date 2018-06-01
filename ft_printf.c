8408da1ce614b0da55121d82d6d6b1cbeac948ab
Malik Jean <maljean@e1z3r3p18.42.us.org>
2018-05-31 19:12:44 -0700

    Moved width and precision parsing to its own functions

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: maljean <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/29 23:48:08 by maljean           #+#    #+#             */
-/*   Updated: 2018/05/31 19:08:35 by maljean          ###   ########.fr       */
+/*   Updated: 2018/05/31 19:12:28 by maljean          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -598,35 +598,35 @@ void	ft_flag_save(char *s, t_ops *ops, int *i)
 	}
 }
 
-void	ft_prec_width_parse(char *s, int i, t_ops ops, va_list args)
+void	ft_prec_width_parse(char *s, int *i, t_ops *ops, va_list args)
 {
-	if (s[i] && (((s[i] > '0' && s[i] <= '9')) || (s[i] == '*' && s[i - 1] != '*')))
+	if (s[*i] && (((s[*i] > '0' && s[*i] <= '9')) || (s[*i] == '*' && s[*i - 1] != '*')))
 	{
-		if (s[i] == '*' && s[i - 1] != '*')
+		if (s[*i] == '*' && s[*i - 1] != '*')
 		{
-			ops.width = va_arg(args, int);
-			i += 1;
+			ops->width = va_arg(args, int);
+			*i += 1;
 		}
 		else
 		{
-			ops.w += 1;
-			ops.width = (ft_atoi(&s[i]));
-			i += ft_nbrlen(ft_atoi(&s[i]));
+			ops->w += 1;
+			ops->width = (ft_atoi(&s[*i]));
+			*i += ft_nbrlen(ft_atoi(&s[*i]));
 		}
 	}
-	if (s[i] && (s[i] == '.'))
+	if (s[*i] && (s[*i] == '.'))
 	{
-		i += 1;
-		if (s[i] == '*')
+		*i += 1;
+		if (s[*i] == '*')
 		{
-			ops.prec = va_arg(args, int);
-			i += 1;
+			ops->prec = va_arg(args, int);
+			*i += 1;
 		}
 		else
 		{
-			ops.p += 1;
-			ops.prec = (ft_atoi(&s[i]));
-			i += ft_nbrlen(ft_atoi(&s[i]));
+			ops->p += 1;
+			ops->prec = (ft_atoi(&s[*i]));
+			*i += ft_nbrlen(ft_atoi(&s[*i]));
 		}
 	}
 }
@@ -635,7 +635,7 @@ int	ft_parse(char *s, int *i, t_ops *ops, va_list args)
 {
 	*i += 1;
 	ft_flag_save(s, ops, i);
-	ft_prec_width_parse(s, *i, *ops, args);
+	ft_prec_width_parse(s, i, ops, args);
 	if (ft_mod_check(s, i, ops) == -1)
 		return (0);
 	if (ft_conv_check(-1, "sSpdDioOuUxXcC", s[*i]))
