df09675b67f9d0b8c018887cc1f2b44b20b53313
Malik Jean <maljean@e1z3r3p18.42.us.org>
2018-06-01 01:52:22 -0700

    Working on leaks

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: maljean <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/29 23:48:08 by maljean           #+#    #+#             */
-/*   Updated: 2018/06/01 01:51:03 by maljean          ###   ########.fr       */
+/*   Updated: 2018/06/01 01:51:55 by maljean          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -416,7 +416,6 @@ char	*ft_zeros(char *s, t_ops *ops)
 char	*ft_spaces(char *s, t_ops *ops)
 {
 	char	*block;
-	char	*ptr;
 	int		i;
 
 	i = 0;
@@ -430,10 +429,8 @@ char	*ft_spaces(char *s, t_ops *ops)
 	}
 	block[i] = '\0';
 	s = (ops->minus) ? ft_strjoin(s, block) : ft_strjoin(block, s);
-	ptr = s;
 	free(block);
-	free(s);
-	return (ptr);
+	return (s);
 }
 
 char	*ft_chop(char *s, t_ops *ops)
