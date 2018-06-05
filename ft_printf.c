ed36271da4c9e12f5b37f8ed40d34c606c57c6db
Malik Jean <maljean@e1z3r5p18.42.us.org>
2018-06-04 23:51:56 -0700

    Working on big s and long s

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: maljean <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/29 23:48:08 by maljean           #+#    #+#             */
-/*   Updated: 2018/06/03 00:26:05 by maljean          ###   ########.fr       */
+/*   Updated: 2018/06/04 23:50:05 by maljean          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -361,6 +361,8 @@ char	*ft_otoa(unsigned long int number, t_ops *ops)
 	i = 0;
 	ops->num += 1;
 	print = (char*)malloc(sizeof(char) * 24);
+	if (number == 0)
+		return ("0");
 	if (number < i)
 		return ("errno: Unsigned Only!");
 	if (number == 0)
@@ -537,11 +539,6 @@ char	*ft_hex_cast(va_list args, t_ops *ops, int base)
 		return (ft_itoabase_umax(va_arg(args, intmax_t), base, ops));
 }
 
-void	ft_putwchar(wchar_t a)
-{
-	write(1, &a, 1);
-}
-
 void	ft_putwstr(wchar_t *ws, t_ops *ops)
 {
 	int i;
@@ -549,10 +546,45 @@ void	ft_putwstr(wchar_t *ws, t_ops *ops)
 	i = 0;
 	while (ws[i] != '\0')
 	{
-		ft_putwchar(ws[i]);
+		write(1, &ws[i], 1);
 		i++;
 		ops->bytes += 1;
 	}
+}
+
+int	ft_print_unicode(wchar_t *wide, t_ops *ops)
+{
+	wchar_t	*fake_wide;
+	int	len;
+	char	org;
+	int	i;
+
+	i = 0;
+	len = 0;
+	org = 0;
+	fake_wide = NULL;
+	if (len == 1)
+		fake_wide[0] = org;
+	else if (len == 2)
+	{
+		fake_wide[0] = ((org >> 6) | 0xC0);
+		fake_wide[1] = ((org & 0x3f) | 0x80);
+	}
+	else if (len == 3)
+	{
+		fake_wide[0] = ((org >> 12) | 0xE0);
+		fake_wide[1] = (((org >> 6) & 0x3f) | 0x80);
+		fake_wide[2] = ((org & 0x3f) | 0x80);
+	}
+	else if (len == 4)
+	{
+		fake_wide[0] = ((org >> 18) | 0xf0);
+		fake_wide[1] = (((org >> 12) & 0x3f) | 0x80);
+		fake_wide[2] = (((org >> 6) & 0x3f) | 0x80);
+		fake_wide[3] = ((org & 0x3f) | 0x80);
+	}
+	ft_putwstr(wide, ops);
+	return (len);
 }
 
 wchar_t	*ft_wchrtostr(wchar_t wchar)
@@ -584,8 +616,8 @@ void	ft_handle_it(t_ops *ops, va_list args)
 		ft_apply_flags(va_arg(args, char*), ops);
 	else if (ops->conv == 'D')
 		ft_apply_flags(ft_itoabase_umax(va_arg(args, long), 10, ops), ops);
-	else if (ops->conv == 'S' || (ops->conv == 'S' && ops->mod[0] == 'l'))
-		ft_putwstr(va_arg(args, wchar_t*), ops);
+	else if (ops->conv == 'S' || (ops->conv == 's' && ops->mod[0] == 'l'))
+		ft_print_unicode(va_arg(args, wchar_t*), ops);
 	else if (ops->conv == 'C')
 		ft_putwstr((ft_wchrtostr(va_arg(args, wchar_t))), ops);
 	else if (ops->conv == 'c')
