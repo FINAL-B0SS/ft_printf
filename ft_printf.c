6300e55ff25be68bb565a0635a41b5b41fe9c875
Malik Jean <maljean@e1z3r5p18.42.us.org>
2018-06-05 00:55:46 -0700

    Handling edge case for precision

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: maljean <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/29 23:48:08 by maljean           #+#    #+#             */
-/*   Updated: 2018/06/05 00:52:02 by maljean          ###   ########.fr       */
+/*   Updated: 2018/06/05 00:55:41 by maljean          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -480,6 +480,7 @@ void	ft_apply_flags(char *s, t_ops *ops)
 	(s[0] == '-') ? ops->prec += 1 : 0;
 	if (!ops->num)
 		s = (ops->prec) ? ft_chop(s, ops) : s;
+	(ops->num && ops->prec <= ft_strlen(s)) ? ops->zero = 0 : 0;
 	(ops->space && s[0] != '-') ? ops->width -= 1 : 0;
 	ops->prec -= ft_strlen(s);
 	(ops->plus && s[0] != '-') ? ops->width -= 1 : 0;
@@ -744,7 +745,7 @@ int	ft_printf(const char *format, ...)
 /*
 int main()
 {
-	printf("%04.2i", 42);
+	ft_printf("%04.2i", 42);
 //  ft_printf("%d\n", 42);
 //  ft_printf("%d%d\n", 42, 41);
 //  ft_printf("%d%d%d\n", 42, 43, 44);
