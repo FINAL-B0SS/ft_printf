0511cb704ec069e4492f5c034c78dad9ca60fb8c
Malik Jean <maljean@e1z3r5p18.42.us.org>
2018-06-05 20:32:23 -0700

    Working on leaks

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: maljean <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/29 23:48:08 by maljean           #+#    #+#             */
-/*   Updated: 2018/06/05 20:31:15 by maljean          ###   ########.fr       */
+/*   Updated: 2018/06/05 18:37:41 by maljean          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -407,11 +407,12 @@ char	*ft_ptoa(unsigned long int number, t_ops *ops)
 
 char	*ft_zeros(char *s, t_ops *ops)
 {
-	char	block[(ops->prec) ? ops->prec + 1 : 1];
+	char	*block;
 	int		i;
 
 	i = 0;
-	if (ops->prec < 1)
+	block = (char*)malloc(sizeof(char) * ops->prec + 1);
+	if (!block)
 		return (s);
 	while (i < ops->prec)
 	{
@@ -427,6 +428,7 @@ char	*ft_zeros(char *s, t_ops *ops)
 	else
 		s = ft_strjoin(block, s);
 	ops->zero = 0;
+	free(block);
 	return (s);
 }
 
@@ -494,7 +496,7 @@ void	ft_apply_flags(char *s, t_ops *ops)
 	s = ft_spaces(s, ops);
 	s = (ops->pound && ops->conv == 'x' && ops->zero) ? ft_strjoin("0x", s) : s;
 	s = (ops->pound && ops->conv == 'X' && ops->zero) ? ft_strjoin("0X", s) : s;
-	(ops->space && s[0] != '-') ? ft_putchar(' ', ops) : s;
+	s = (ops->space && s[0] != '-') ? ft_strjoin(" ", s) : s;
 	ft_putstr(s, ops);
 }
 
@@ -747,36 +749,8 @@ int	ft_printf(const char *format, ...)
 	return (bytes);
 }
 /*
-int main(void)
+int main()
 {
-//  ft_printf("%d\n", 42);
-//  ft_printf("%d%d\n", 42, 41);
-//  ft_printf("%d%d%d\n", 42, 43, 44);
-//  ft_printf("%ld\n", 2147483647);
-//  ft_printf("%lld\n", 9223372036854775807);
-//  ft_printf("%x\n", 505);
-//  ft_printf("%X\n", 505);
-//  ft_printf("%p\n", &ft_printf);
-// ft_printf("%20.15d\n", 54321);
-//  ft_printf("%-10d\n", 3);
-//-- ft_printf("% d\n", 3);
-//  ft_printf("%+d\n", 3);
-//  ft_printf("%010d\n", 1);
-//  ft_printf("%hhd\n", 0);
-//  ft_printf("%jd\n", 9223372036854775807);
-//  ft_printf("%zd\n", 4294967295);
-//  ft_printf("%\n");
-//  ft_printf("%U\n", 4294967295);
-//  ft_printf("%u\n", 4294967295);
-// ft_printf("%o\n", 40);
-//  ft_printf("%%#08x\n", 42);
-//  ft_printf("%x\n", 1000);
-//  ft_printf("%#X\n", 1000);
-//  ft_printf("%s\n", NULL);
-//  ft_printf("%S\n", L"ݗݜशব");
-//  ft_printf("%s%s\n", "test", "test");
-//  ft_printf("%s%s%s\n", "test", "test", "test");
-//  ft_printf("%C\n", 15000);
-  ft_printf("%5.3d\n", 42);
-  return (0);
+	ft_printf("%#08x", 42);
+	return (0);
 }*/
