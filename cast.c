82ae6bdcd1c2fc7fc026007b3544915fa93e63dd
Malik Jean <maljean@e1z3r5p18.42.us.org>
2018-06-07 23:14:19 -0700

    Super norme-al

--- cast.c
+++ cast.c
@@ -6,7 +6,7 @@
 /*   By: maljean <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/06 02:12:37 by maljean           #+#    #+#             */
-/*   Updated: 2018/06/07 23:12:50 by maljean          ###   ########.fr       */
+/*   Updated: 2018/06/07 23:14:14 by maljean          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -75,7 +75,8 @@ void	ft_putwstr(wchar_t *ws, t_ops *ops, int i)
 {
 	ops->width -= (ops->prec -ft_wstrlen(ws));
 	ops->minus ? ops->zero = 0 : 0;
-	if (ops->width > 0 && !ops->minus--)
+	ops->width -= 1;
+	if (ops->width && !ops->minus)
 		while (ops->width)
 			ops->zero ? write(1, "0", 1) : write(1, " ", 1);
 	while (ws[++i])
@@ -89,7 +90,7 @@ void	ft_putwstr(wchar_t *ws, t_ops *ops, int i)
 		ft_putchar(ws[i], ops);
 		ops->bytes += 1;
 	}
-	if (ops->width > 0 && ops->minus)
+	if (ops->width && ops->minus)
 		while (ops->width--)
 			ops->zero ? write(1, "0", 1) : write(1, " ", 1);
 }
